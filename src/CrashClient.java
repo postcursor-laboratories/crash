b87174ad37bc4af435f3111d73bb1a06c6b836b5
Timeroot <timeroot.alex@gmail.com>
2016-01-01 17:06:38 -0800

    Huh?
    how do I work this github thing

--- src/CrashClient.java
+++ src/CrashClient.java
@@ -39,15 +39,11 @@ public class CrashClient {
 	Canvas can;
 
 	Socket serv;
-	DataOutputStream servOut;
-	DataInputStream servIn;
-	
-	static final int W = 1000, H = 800;
-	static final Color MAROON = new Color(120, 0, 160);
+	Player player;
+
+	static final int W = Resources.W, H = Resources.H;
 
 	boolean[] keys = new boolean[1 << 16];
-	Font font;
-	Image bg,fogratImg,woosterImg;
 	
 	long lostTime;
 
@@ -62,35 +58,14 @@ public class CrashClient {
 		jf.setLocation(800, 300);
 
 		try {
-			serv=new Socket("184.187.175.50",42973);
-			servIn=new DataInputStream(serv.getInputStream());
-			System.out.println(serv.getInputStream().getClass());
-			servOut=new DataOutputStream(serv.getOutputStream());
+			serv=new Socket("localhost",42973);
+			player = new Player(
+					new DataOutputStream(serv.getOutputStream()),
+					new DataInputStream(serv.getInputStream())
+							);
 		} catch (IOException e2) {
 			e2.printStackTrace();
 			return;
-		}
-		
-		try {
-			InputStream is = CrashServer.class.getClassLoader().getResourceAsStream(
-					"ScoreFont.ttf");
-			font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(50f);
-			System.out.println(font);
-			is.close();
-		} catch (IOException | FontFormatException ex) {
-			ex.printStackTrace();
-			System.err.println("Font absent from res!");
-			font = Font.decode("Arial 12");
-		}
-
-		try {
-			bg = ImageIO.read(CrashServer.class.getClassLoader().getResourceAsStream(
-					"sunset-bg.png"));
-		} catch (IOException | IllegalArgumentException e1) {
-			bg = new BufferedImage(W, H, BufferedImage.TYPE_INT_RGB);
-			Graphics g=bg.getGraphics();
-			g.setColor(MAROON);
-			g.fillRect(0, 0, W, H);//
 		}
 
 		can.addKeyListener(new KeyListener() {
@@ -110,7 +85,7 @@ public class CrashClient {
 
 		world = new GameWorld();
 		try {
-			world.readWorld(servIn);
+			world.readWorld(player.cliIn);
 		} catch (IOException e1) {
 			throw new RuntimeException(e1);
 		}
@@ -119,7 +94,8 @@ public class CrashClient {
 			public void run(){
 				try {
 					while(true){
-						world.readWorld(servIn);
+						world.readWorld(player.cliIn);
+						player.sendKeys();
 					} 
 				} catch (IOException e1) {
 					throw new RuntimeException(e1);
@@ -145,14 +121,6 @@ public class CrashClient {
 			}
 		}
 	}
-	
-	void sendKeys() throws IOException{
-		servOut.writeBoolean(keys[KeyEvent.VK_W]);
-		servOut.writeBoolean(keys[KeyEvent.VK_A]);
-		servOut.writeBoolean(keys[KeyEvent.VK_S]);
-		servOut.writeBoolean(keys[KeyEvent.VK_D]);
-	}
-	
 
 	public static void main(String[] args) {
 		if(args.length > 0){
